a4f8f7a189b4d50abed7a125e11820baf5aa7da0
pd-mishra <pd.mishra@gmail.com>
2021-01-28 08:32:23 +0530

    committing before adding manage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/stocks.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
+//let service: string = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
+//let apiKey: string = ' IHLGIQODOLKV70FP';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
+@Injectable()
+export class StocksService {
+
+  constructor(private http: HttpClient) { }
+
+  get() {
+    return stocks.slice();
+  }
+
+  add(stock: string) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock: string) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols) {
+    if(symbols) {
+      return this.http.get<Array<StockInterface>>(`${service}/stocks/snapshot?symbols=${symbols.join()}`)
+    }
+  }
+}
